92e35b96f7cd1b7b320937cac226d37a5d95207b
ayushhhkha <90859224+ayushhhkha@users.noreply.github.com>
2024-06-23 17:53:15 +0200

    Revert "Amir's branch merge with main"

--- sanic/app.py
+++ sanic/app.py
@@ -114,6 +114,7 @@ ctx_type = TypeVar("ctx_type")
 config_type = TypeVar("config_type", bound=Config)
 
 
+
 ack_branch_coverage = {
     "ack_has_multiplexer" : False,
     "ack_no_multiplexer" : False
@@ -123,7 +124,7 @@ ack_branch_coverage = {
 unregister_branches = {"not_an_instance": False, "name_in_registry": False}
 
 
-refresh_branch_coverage = {"refresh_b1" : False, "refresh_b2" : False, "refresh_b3" : False, "refresh_b4" : False, "refresh_b5" : False}
+refresh_branch_coverage = {"refresh_b1" : False, "refresh_b2" : False, "refresh_b3" : False, "refresh_b4" : False}
 purge_branch_coverage = {"purge_b1" : False, "purge_b2" : False}
 
 
@@ -1896,12 +1897,6 @@ class Sanic(
             k: v for k, v in self._task_registry.items() if v is not None
         }
 
-
-    def print_purge_coverage():
-        for branch, hit in purge_branch_coverage.items():
-            print(f"{branch} was {'hit' if hit else 'not hit'}")
-
-
     def shutdown_tasks(
         self, timeout: Optional[float] = None, increment: float = 0.1
     ) -> None:
@@ -2527,23 +2522,11 @@ class Sanic(
         if passthru:
             for attr, info in passthru.items():
                 if isinstance(info, dict):
-                    refresh_branch_coverage["refresh_b4"] = True
                     for key, value in info.items():
                         setattr(getattr(self, attr), key, value)
                 else:
                     setattr(self, attr, info)
         if hasattr(self, "multiplexer"):
-
-            refresh_branch_coverage["refresh_b5"] = True
-            self.shared_ctx.lock()
-        return self
-
-    def print_refresh_coverage():
-        for branch, hit in refresh_branch_coverage.items():
-            print(f"{branch} was {'hit' if hit else 'not hit'}")
-    
-
-=======
             self.shared_ctx.lock()
         return self
 
